e4492f7edcc463cf995db579d04c53197d90aa33
KorrawitF <korrawit@The-Prodigy-PDG-COM-00-019.local>
2026-05-30 22:06:36 +0700

    feat: add Friend service and implement SendInvite functionality with logging

--- services/friend/friend.cpp
+++ services/friend/friend.cpp
@@ -1,6 +1,7 @@
 #include "friend.h"
 #include "discordpp.h"
 #include <string>
+#include <iostream>
 
 Friend::Friend(std::shared_ptr<discordpp::Client> *Client) : client(*Client) {}
 
@@ -8,8 +9,10 @@ bool Friend::SendInvite(uint64_t userId, const std::string &message) {
     bool ret;
     client->SendActivityInvite(userId, message, [&ret](discordpp::ClientResult result) {
         if (result.Successful()) {
+            std::cout << "Invitation sent successfully." << std::endl;
             ret = true;
         } else {
+            std::cout << "Failed to send invitation, err: " + result.Error() << std::endl;
             ret = false;
         } 
     });
